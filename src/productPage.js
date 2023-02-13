6a4d733e09a2ecefb849dd83154fdcca249e3294
Ahmed Al Qady <26540442+ABQady@users.noreply.github.com>
2023-02-13 18:41:45 +0200

    deleteall async

--- src/productPage.js
+++ src/productPage.js
@@ -34,35 +34,36 @@ class NavBar extends Component {
 
    render() {
 
-      function deleteAll() {
-         let checkboxes = document.getElementsByClassName('form-check-input');
+      deleteAll = async () => {
+         {
+            let checkboxes = document.getElementsByClassName('form-check-input');
 
-         for (var i = 0; i < checkboxes.length; i++) {
-            if (checkboxes[i].checked == true) {
-               Axios.post('https://server-abqady.vercel.app/delete', { SKU: checkboxes[i].value });
-               console.log(checkboxes[i].value)
+            for (var i = 0; i < checkboxes.length; i++) {
+               if (checkboxes[i].checked == true) {
+                  await Axios.post('https://server-abqady.vercel.app/delete', { SKU: checkboxes[i].value });
+                  console.log(checkboxes[i].value);
+               }
             }
+            // wait(2);
+            window.location.href = '/';
          }
-         // wait(2);
-         // window.location.href = '/';
-      }
-      return (
-         <div>
-            <div className='d-flex flex-row pt-5'>
-               <div className='col-6 my-auto'>
-                  <h1 className='ps-3'>Product List</h1>
-               </div>
-               <div className='d-flex justify-content-end col-6 my-auto'>
-                  <a href='/add-product' className='btn btn-outline-primary me-3'> ADD</a>
-                  <button onClick={deleteAll} className="btn btn-outline-primary me-3 delete-checkbox"> MASS DELETE</button>
-               </div>
-            </div>
+         return (
             <div>
-               <hr className='m-3'></hr>
-            </div>
-         </div >
-      );
+               <div className='d-flex flex-row pt-5'>
+                  <div className='col-6 my-auto'>
+                     <h1 className='ps-3'>Product List</h1>
+                  </div>
+                  <div className='d-flex justify-content-end col-6 my-auto'>
+                     <a href='/add-product' className='btn btn-outline-primary me-3'> ADD</a>
+                     <button onClick={deleteAll} className="btn btn-outline-primary me-3 delete-checkbox"> MASS DELETE</button>
+                  </div>
+               </div>
+               <div>
+                  <hr className='m-3'></hr>
+               </div>
+            </div >
+         );
+      }
    }
-}
 
 export default ProductPage;
