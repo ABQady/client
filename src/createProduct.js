0e56891adebbab2087cf2846f6bb641f275ae77d
Ahmed Al Qady <26540442+ABQady@users.noreply.github.com>
2023-02-13 17:02:06 +0200

    Adjustments for remote host

--- src/createProduct.js
+++ src/createProduct.js
@@ -94,7 +94,7 @@ function CreateProduct() {
       setDimensions(dim);
       console.log(dimensions)
       if (validateForm()) {
-         Axios.post('http://localhost:3002/api/create', { sku: sku, name: name, price: price, size: size, weight: weight, dimensions: dimensions, type: switcher });
+         Axios.post('https://server-abqady.vercel.app/api/create', { sku: sku, name: name, price: price, size: size, weight: weight, dimensions: dimensions, type: switcher });
          navigate('/', { replace: true });
       }
    }
