18ea089e6539074882b0243668a76c995d7a0fed
Ahmed Al Qady <26540442+ABQady@users.noreply.github.com>
2023-02-22 14:17:15 +0200

    ya rab

--- src/productPage.js
+++ src/productPage.js
@@ -40,8 +40,8 @@ class NavBar extends Component {
             if (checkboxes[i].checked === true) {
                try {
                   //await 
-                  Axios.post('https://server-abqady.vercel.app/delete', { SKU: checkboxes[i].value });
-                  checkboxes[i].classList.add("d-none");
+                  Axios.post('https://server-abqady.vercel.app/delete', { SKU: checkboxes[i].value })
+                     .then(checkboxes[i].classList.add("d-none"))
                   // .then(
                   //    this.setState({
                   //       products: this.state.products.filter(product => product.SKU !== checkboxes[i].value)
