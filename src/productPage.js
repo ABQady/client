9d8e1ed3df63faa3985e141f5bdbe891016b77e7
Ahmed Al Qady <26540442+ABQady@users.noreply.github.com>
2023-02-26 15:32:42 +0200

    a7aaah?

--- src/productPage.js
+++ src/productPage.js
@@ -8,6 +8,44 @@ function ProductPage() {
 
    const [products, setProducts] = useState([]);
 
+   function deleteProduct(sku) {
+      const copy = [...products];
+      copy.splice(sku, 1);
+      setProducts(copy);
+   }
+
+   //async function deleteAll() {
+   const deleteAll = async () => {
+
+      let checkboxes = document.getElementsByClassName('delete-checkbox');
+
+      for (var i = 0; i < checkboxes.length; i++) {
+         if (checkboxes[i].checked === true) {
+            try {
+               await
+                  Axios.post('https://server-abqady.vercel.app/delete', { SKU: checkboxes[i].value })
+                     .then((response) => {
+                        console.log(response.status);
+                        console.log(response.data);
+                     })
+                     //  .then(checkboxes[i].classList.add("d-none"))
+                     // .then(
+                     //    this.setState({
+                     //       products: this.state.products.filter(product => product.SKU !== checkboxes[i].value)
+                     //    })
+                     // );
+                     //.then(this.setProducts(this.state.products.filter(product => product.SKU !== checkboxes[i].value)))
+                     .then(deleteProduct(checkboxes[i].value))
+            } catch (e) {
+
+            }
+            console.log(checkboxes[i].value);
+         }
+      }
+      // document.getElementsById("delete-checkbox").classList.add("d-none");
+      //window.location.reload();
+   }
+
    useEffect(async () => {
       await Axios.get("https://server-abqady.vercel.app/get").then((response) => {
          setProducts(response.data)
@@ -16,61 +54,7 @@ function ProductPage() {
 
    return (
       <div>
-         <NavBar />
-         <div className="row justify-content-center">
-            {products.map((product) => {
-               return (
-                  <Product {...product} />
-               )
-            })}
-         </div>
-         <Footer />
-      </div>
-   );
-}
-
-class NavBar extends Component {
-
-   render() {
-      function deleteProduct(sku) {
-         const copy = [...this.products];
-         copy.splice(sku, 1);
-         this.setProducts(copy);
-      }
-
-      //async function deleteAll() {
-      const deleteAll = async () => {
-
-         let checkboxes = document.getElementsByClassName('delete-checkbox');
-
-         for (var i = 0; i < checkboxes.length; i++) {
-            if (checkboxes[i].checked === true) {
-               try {
-                  await
-                     Axios.post('https://server-abqady.vercel.app/delete', { SKU: checkboxes[i].value })
-                        .then((response) => {
-                           console.log(response.status);
-                           console.log(response.data);
-                        })
-                        //  .then(checkboxes[i].classList.add("d-none"))
-                        // .then(
-                        //    this.setState({
-                        //       products: this.state.products.filter(product => product.SKU !== checkboxes[i].value)
-                        //    })
-                        // );
-                        //.then(this.setProducts(this.state.products.filter(product => product.SKU !== checkboxes[i].value)))
-                        .then(deleteProduct(checkboxes[i].value))
-               } catch (e) {
-
-               }
-               console.log(checkboxes[i].value);
-            }
-         }
-         // document.getElementsById("delete-checkbox").classList.add("d-none");
-         //window.location.reload();
-      }
-
-      return (
+         {/* <NavBar /> */}
          <div>
             <div className='d-flex flex-row pt-5'>
                <div className='col-6 my-auto'>
@@ -85,8 +69,77 @@ class NavBar extends Component {
                <hr className='m-3'></hr>
             </div>
          </div >
-      );
-   }
+
+         <div className="row justify-content-center">
+            {products.map((product) => {
+               return (
+                  <Product {...product} />
+               )
+            })}
+         </div>
+         <Footer />
+      </div>
+   );
 }
+
+// class NavBar extends Component {
+
+//    render() {
+//       // function deleteProduct(sku) {
+//       //    const copy = [...this.products];
+//       //    copy.splice(sku, 1);
+//       //    this.setProducts(copy);
+//       // }
+
+//       // //async function deleteAll() {
+//       // const deleteAll = async () => {
+
+//       //    let checkboxes = document.getElementsByClassName('delete-checkbox');
+
+//       //    for (var i = 0; i < checkboxes.length; i++) {
+//       //       if (checkboxes[i].checked === true) {
+//       //          try {
+//       //             await
+//       //                Axios.post('https://server-abqady.vercel.app/delete', { SKU: checkboxes[i].value })
+//       //                   .then((response) => {
+//       //                      console.log(response.status);
+//       //                      console.log(response.data);
+//       //                   })
+//       //                   //  .then(checkboxes[i].classList.add("d-none"))
+//       //                   // .then(
+//       //                   //    this.setState({
+//       //                   //       products: this.state.products.filter(product => product.SKU !== checkboxes[i].value)
+//       //                   //    })
+//       //                   // );
+//       //                   //.then(this.setProducts(this.state.products.filter(product => product.SKU !== checkboxes[i].value)))
+//       //                   .then(deleteProduct(checkboxes[i].value))
+//       //          } catch (e) {
+
+//       //          }
+//       //          console.log(checkboxes[i].value);
+//       //       }
+//       //    }
+//       //    // document.getElementsById("delete-checkbox").classList.add("d-none");
+//       //    //window.location.reload();
+//       // }
+
+//       return (
+//          // <div>
+//          //    <div className='d-flex flex-row pt-5'>
+//          //       <div className='col-6 my-auto'>
+//          //          <h1 className='ps-3'>Product List</h1>
+//          //       </div>
+//          //       <div className='d-flex justify-content-end col-6 my-auto'>
+//          //          <a href='/add-product' className='btn btn-outline-primary me-3'> ADD</a>
+//          //          <button onClick={() => { deleteAll(); }} className="btn btn-outline-primary me-3 delete-checkbox"> MASS DELETE</button>
+//          //       </div>
+//          //    </div>
+//          //    <div>
+//          //       <hr className='m-3'></hr>
+//          //    </div>
+//          // </div >
+//       );
+//    }
+// }
 
 export default ProductPage;
