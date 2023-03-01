e1663b1aaa15345883f7768f3896b4554dd3438e
Ahmed Al Qady <26540442+ABQady@users.noreply.github.com>
2023-03-01 16:09:48 +0200

    do i need timout before navigating?

--- src/createProduct.js
+++ src/createProduct.js
@@ -101,10 +101,11 @@ function CreateProduct() {
       if (validateForm()) {
          try {
             const response = await Axios.post('https://server-abqady.vercel.app/create', { sku: sku, name: name, price: price, size: size, weight: weight, dimensions: dimensions, type: switcher })
+               // .then(
+               //    await timeout(1000))
                .then(
-                  await timeout(1000)).then(
-                     navigate('/', { replace: true })
-                  );
+                  navigate('/', { replace: true })
+               );
          } catch (e) {
             console.log(e);
          }
